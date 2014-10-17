641719287736871be08b181f2f0ab1904cce2177
anotherangrymouse <grillopress.com@gmail.com>
2014-10-17 11:42:16 +0000

    set up inital concept

--- js/laces.js
+++ js/laces.js
@@ -2,3 +2,13 @@
 // Copyright 2014 Andrew Duckworth, under the Apache 2
 // https://github.com/gitpress/laces
 //
+
+$(document).ready(function() {
+  "use strict";
+  
+  $( "div" ).filter( ".col-md-12" ).toggleClass("col-md-10");
+  
+  $( "div" ).filter( ".col-md-6" ).toggleClass("col-md-5"); 
+  
+  $( "div" ).filter( ".col-md-8" ).toggleClass("col-md-6");  
+});
